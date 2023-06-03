d39f5c2ead28c1b6c0278472c878a374143f1320
DMMWEBCAMPGitHub <130573254+DMMWEBCAMPGitHub@users.noreply.github.com>
2023-06-03 15:59:21 +0000

    [add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
    validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
+   validates :introduction, length: { maximum: 50 }
 
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
