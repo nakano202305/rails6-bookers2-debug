2621f3b21a6e6771078f0a1181d6ff235ce1836e
DMMWEBCAMPGitHub <130573254+DMMWEBCAMPGitHub@users.noreply.github.com>
2023-06-06 04:43:23 +0000

    [Finish]4

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,15 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
   # 一覧画面で使う
   has_many :followings, through: :relationships, source: :followed
-  has_many :followers, through: :relationships, source: :follower
-  
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   # フォローしたときの処理
   def follow(user_id)
     relationships.create(followed_id: user_id)
