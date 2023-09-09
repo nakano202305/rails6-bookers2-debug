641d5b56f5641edfb9f8c3a0a92a16ccaf6c6932
DMMWEBCAMPGitHub <130573254+DMMWEBCAMPGitHub@users.noreply.github.com>
2023-09-09 12:27:32 +0000

    [Update]search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,8 +5,10 @@ class SearchesController < ApplicationController
 
     if @range == "User"
        @users = User.looks(params[:search], params[:word])
+       render "/searches/search_result"
     else
        @books = Book.looks(params[:search], params[:word])
+       render "/searches/search_result"
     end
   end
   
