35953e53ed2b7045cf267e80b781a27f5f4e5a7a
DMMWEBCAMPGitHub <130573254+DMMWEBCAMPGitHub@users.noreply.github.com>
2023-09-11 13:56:05 +0000

    [Fix]search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,13 +53,13 @@ class UsersController < ApplicationController
 
   def search
     @user = User.find(params[:user_id])
-    @books = @user.books 
+    @books = @user.books
     @book = Book.new
     if params[:created_at] == ""
-      @search_book = "日付を選択してください"#①
+      @search_book = "日付を選択してください"
     else
       create_at = params[:created_at]
-      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count#②
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
     end
   end
 
